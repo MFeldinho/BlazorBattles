9122f29c5fbf093377822f0ff14de3baaf18441d
Marcel Felder <m.felder@live.de>
2021-12-04 10:59:56 +0100

    Updates

---
+++ BlazorBattles/Client/Services/IUnitService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorBattles.Shared;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface IUnitService
+    {
+        IList<Unit> Units { get;}
+
+        IList<UserUnit> MyUnits { get; }
+
+        void AddUnit(int unitId);
+    }
+}
